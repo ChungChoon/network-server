a6872b374248e9edec1514e166f5d5947e82d0b0
배지원 <bjiwon2005@naver.com>
2018-11-10 20:41:23 +0900

    add set network server

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const express = require('express'),
 
 /* GET home page. */
 router.post('/', upload.single('keyFile'), async (req, res, next) => {
-        console.log(req.file);
+        console.log(req.body);
         res.status(200).json({
             message: "regOK",
-            name: req.file.originalname
+            // name: req.file.originalname
         })
     });
 
