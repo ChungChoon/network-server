d157c3251a76c4676807c87d5a1167d194c4aa51
배지원 <bjiwon2005@naver.com>
2018-11-10 21:23:16 +0900

    add set network server

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,33 @@
 
 const express = require('express'),
   router = express.Router(),
-    upload = require('../config/multer').upload;
+    upload = require('../config/multer').upload,
+    fs = require('fs');
 
-/* GET home page. */
-router.post('/', upload.single('keyFile'), async (req, res, next) => {
-        console.log(req.body);
+
+router.post('/', async (req, res, next) => {
+    console.log(req.body);
+    fs.writeFile("bc_network/data/dd/keystore/test3.json", req.body.keyFile, function(err){
+        if (err) throw err;
+        console.log("success");
         res.status(200).json({
             message: "regOK",
             // name: req.file.originalname
         })
     });
+    // res.status(200).json({
+    //     message: "regOK",
+    //     // name: req.file.originalname
+    // })
+});
+/* GET home page. */
+// router.post('/', upload.single('keyFile'), async (req, res, next) => {
+//         console.log(req.body);
+//         res.status(200).json({
+//             message: "regOK",
+//             // name: req.file.originalname
+//         })
+//     });
 
 module.exports = router;
 
